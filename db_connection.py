171e0a4772b53e51bc9e64f19660a38220975e5a
nogapazz <134314972+nogapazz@users.noreply.github.com>
2023-09-23 15:18:39 +0300

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -12,9 +12,9 @@ def db_connector(db_name):
     mysql = MySQL()
     # Setting up configurations to form the connection:
     app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'  #host
-    app.config['MYSQL_DATABASE_USER'] = 'root'  #username
-    app.config['MYSQL_DATABASE_PASSWORD'] = 'root' #password
-    app.config['MYSQL_DATABASE_DB'] = 'group31_db' #database name
+    app.config['MYSQL_DATABASE_USER'] = 'your sql username'  #username
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'your sql password' #password
+    app.config['MYSQL_DATABASE_DB'] = 'your scheme' #database name
     # Initializing the app:
     mysql.init_app(app)
     # Connect to the database
